e426b6f1ea31353d6e03a987cedc6a1eb530596e
yiran2014 <781107407@qq.com>
2018-04-22 22:47:57 +0800

    split

--- code_split/index.js
+++ code_split/index.js
@@ -6,5 +6,5 @@ console.log('I am index')
 
 // require.ensure(dependencies: String[], callback: function(require), chunkName: String)
 
-require('./split');
-// import * as Test from './split.js';
+require('./split');  //通过commonjs的机制拿到split.js
+// import * as Test from './split.js';   //webpack2也是原生支持import语法的，webpack1不支持
